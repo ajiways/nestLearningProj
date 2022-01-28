d1dd84919dde1a9b4dfe86222fd8baec2aa3fb40
ajiways <ajiwayswc@gmail.com>
2022-01-29 01:52:27 +0700

    package.json app.module и main

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,43 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { AttachmentModule } from './modules/attachment/attachment.module';
+import { Connection } from 'typeorm';
+import { ConfigModule } from '@nestjs/config';
+import { AuthModule } from './modules/auth/auth.module';
+import { TokenModule } from './modules/token/token.module';
+import { BrandModule } from './modules/brand/brand.module';
+import { CartModule } from './modules/cart/cart.module';
+import { CategoryModule } from './modules/category/category.module';
+import { CurrencyModule } from './modules/currency/currency.module';
+import { OrderModule } from './modules/order/order.module';
+import { PaymentModule } from './modules/payment/payment.module';
+import { ProductModule } from './modules/product/product.module';
+import { ProductImageModule } from './modules/product-image/product-image.module';
+import { PropertyModule } from './modules/property/property.module';
+import { typeOrmConfigAsync } from './config/typeorm';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRootAsync(typeOrmConfigAsync),
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    AttachmentModule,
+    AuthModule,
+    TokenModule,
+    BrandModule,
+    CartModule,
+    CategoryModule,
+    CurrencyModule,
+    OrderModule,
+    PaymentModule,
+    ProductModule,
+    ProductImageModule,
+    PropertyModule,
+  ],
   controllers: [],
   providers: [],
 })
-export class AppModule {}
+export class AppModule {
+  constructor(private connection: Connection) {}
+}
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,17 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as cookieParser from 'cookie-parser';
+import { config } from 'dotenv';
+import { ConfigService } from '@nestjs/config';
+import { getConnection } from 'typeorm';
+config();
 
 async function bootstrap() {
+  const configService = new ConfigService();
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  app.use(cookieParser());
+  await app.listen(configService.get('PORT'));
+  const connection = await getConnection();
+  await connection.runMigrations();
 }
 bootstrap();
