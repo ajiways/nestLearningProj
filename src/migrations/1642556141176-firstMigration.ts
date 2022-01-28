6e352be9cc470dcfc1814e4d239448a2abe94a82
ajiways <ajiwayswc@gmail.com>
2022-01-29 01:51:12 +0700

    Миграции

--- src/migrations/1642556141176-firstMigration.ts
+++ src/migrations/1642556141176-firstMigration.ts
@@ -1,11 +1,11 @@
-import { MigrationInterface, QueryRunner } from "typeorm";
+import { MigrationInterface, QueryRunner } from 'typeorm';
 
 export class firstMigration1642556141176 implements MigrationInterface {
-   public async up(queryRunner: QueryRunner): Promise<void> {
-      await queryRunner.query(`
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(`
             CREATE TYPE "public"."attachments_type_enum" AS ENUM('SMALL', 'ORIGINAL')
         `);
-      await queryRunner.query(`
+    await queryRunner.query(`
             CREATE TABLE "attachments" (
                 "id" SERIAL NOT NULL,
                 "type" "public"."attachments_type_enum" NOT NULL,
@@ -14,7 +14,7 @@ export class firstMigration1642556141176 implements MigrationInterface {
                 CONSTRAINT "PK_5e1f050bcff31e3084a1d662412" PRIMARY KEY ("id")
             )
         `);
-      await queryRunner.query(`
+    await queryRunner.query(`
             CREATE TABLE "brands" (
                 "id" SERIAL NOT NULL,
                 "caption" character varying(56) NOT NULL,
@@ -23,7 +23,7 @@ export class firstMigration1642556141176 implements MigrationInterface {
                 CONSTRAINT "PK_b0c437120b624da1034a81fc561" PRIMARY KEY ("id")
             )
         `);
-      await queryRunner.query(`
+    await queryRunner.query(`
             CREATE TABLE "users" (
                 "id" SERIAL NOT NULL,
                 "email" character varying NOT NULL,
@@ -36,10 +36,10 @@ export class firstMigration1642556141176 implements MigrationInterface {
                 CONSTRAINT "PK_a3ffb1c0c8416b9fc6f907b7433" PRIMARY KEY ("id")
             )
         `);
-      await queryRunner.query(`
+    await queryRunner.query(`
             CREATE TYPE "public"."orders_status_enum" AS ENUM('PREPARING', 'REGISTRATION', 'PAYING')
         `);
-      await queryRunner.query(`
+    await queryRunner.query(`
             CREATE TABLE "orders" (
                 "id" SERIAL NOT NULL,
                 "status" "public"."orders_status_enum" NOT NULL DEFAULT 'REGISTRATION',
@@ -49,7 +49,7 @@ export class firstMigration1642556141176 implements MigrationInterface {
                 CONSTRAINT "PK_710e2d4957aa5878dfe94e4ac2f" PRIMARY KEY ("id")
             )
         `);
-      await queryRunner.query(`
+    await queryRunner.query(`
             CREATE TABLE "categories" (
                 "id" SERIAL NOT NULL,
                 "caption" character varying(56) NOT NULL,
@@ -58,7 +58,7 @@ export class firstMigration1642556141176 implements MigrationInterface {
                 CONSTRAINT "PK_24dbc6126a28ff948da33e97d3b" PRIMARY KEY ("id")
             )
         `);
-      await queryRunner.query(`
+    await queryRunner.query(`
             CREATE TABLE "currencies" (
                 "id" SERIAL NOT NULL,
                 "symbol" character varying(3) NOT NULL,
@@ -66,10 +66,10 @@ export class firstMigration1642556141176 implements MigrationInterface {
                 CONSTRAINT "PK_d528c54860c4182db13548e08c4" PRIMARY KEY ("id")
             )
         `);
-      await queryRunner.query(`
+    await queryRunner.query(`
             CREATE TYPE "public"."properties_type_enum" AS ENUM('TEXT', 'NUMBER', 'DATE', 'COLOR')
         `);
-      await queryRunner.query(`
+    await queryRunner.query(`
             CREATE TABLE "properties" (
                 "id" SERIAL NOT NULL,
                 "caption" character varying NOT NULL,
@@ -77,7 +77,7 @@ export class firstMigration1642556141176 implements MigrationInterface {
                 CONSTRAINT "PK_2d83bfa0b9fcd45dee1785af44d" PRIMARY KEY ("id")
             )
         `);
-      await queryRunner.query(`
+    await queryRunner.query(`
             CREATE TABLE "products" (
                 "id" SERIAL NOT NULL,
                 "caption" character varying(56) NOT NULL,
@@ -90,7 +90,7 @@ export class firstMigration1642556141176 implements MigrationInterface {
                 CONSTRAINT "PK_0806c755e0aca124e67c0cf6d7d" PRIMARY KEY ("id")
             )
         `);
-      await queryRunner.query(`
+    await queryRunner.query(`
             CREATE TABLE "carts" (
                 "id" SERIAL NOT NULL,
                 "amount" integer NOT NULL,
@@ -99,7 +99,7 @@ export class firstMigration1642556141176 implements MigrationInterface {
                 CONSTRAINT "PK_b5f695a59f5ebb50af3c8160816" PRIMARY KEY ("id")
             )
         `);
-      await queryRunner.query(`
+    await queryRunner.query(`
             CREATE TABLE "payments" (
                 "id" SERIAL NOT NULL,
                 "kind" character varying(56) NOT NULL,
@@ -108,7 +108,7 @@ export class firstMigration1642556141176 implements MigrationInterface {
                 CONSTRAINT "PK_197ab7af18c93fbb0c9b28b4a59" PRIMARY KEY ("id")
             )
         `);
-      await queryRunner.query(`
+    await queryRunner.query(`
             CREATE TABLE "product_images" (
                 "id" SERIAL NOT NULL,
                 "range" integer NOT NULL,
@@ -117,192 +117,192 @@ export class firstMigration1642556141176 implements MigrationInterface {
                 CONSTRAINT "PK_1974264ea7265989af8392f63a1" PRIMARY KEY ("id")
             )
         `);
-      await queryRunner.query(`
+    await queryRunner.query(`
             CREATE TABLE "product_properties" (
                 "product_id" integer NOT NULL,
                 "property_id" integer NOT NULL,
                 CONSTRAINT "PK_138669e900adf3161b794736298" PRIMARY KEY ("product_id", "property_id")
             )
         `);
-      await queryRunner.query(`
+    await queryRunner.query(`
             CREATE INDEX "IDX_7fad253874cd58cb759099147a" ON "product_properties" ("product_id")
         `);
-      await queryRunner.query(`
+    await queryRunner.query(`
             CREATE INDEX "IDX_0f3885b4091f332d3476181e0c" ON "product_properties" ("property_id")
         `);
-      await queryRunner.query(`
+    await queryRunner.query(`
             CREATE TABLE "cart_products" (
                 "cart_id" integer NOT NULL,
                 "product_id" integer NOT NULL,
                 CONSTRAINT "PK_bcd1ff5a1d4f6ef4e3a00af6886" PRIMARY KEY ("cart_id", "product_id")
             )
         `);
-      await queryRunner.query(`
+    await queryRunner.query(`
             CREATE INDEX "IDX_ebc4fe8eabf38786bb86cda0b9" ON "cart_products" ("cart_id")
         `);
-      await queryRunner.query(`
+    await queryRunner.query(`
             CREATE INDEX "IDX_bb7910594db3f08cadeb6f484c" ON "cart_products" ("product_id")
         `);
-      await queryRunner.query(`
+    await queryRunner.query(`
             ALTER TABLE "brands"
             ADD CONSTRAINT "FK_1d6279240938040736d83064356" FOREIGN KEY ("logo_attachment_id") REFERENCES "attachments"("id") ON DELETE NO ACTION ON UPDATE NO ACTION
         `);
-      await queryRunner.query(`
+    await queryRunner.query(`
             ALTER TABLE "orders"
             ADD CONSTRAINT "FK_772d0ce0473ac2ccfa26060dbe9" FOREIGN KEY ("customer_id") REFERENCES "users"("id") ON DELETE NO ACTION ON UPDATE NO ACTION
         `);
-      await queryRunner.query(`
+    await queryRunner.query(`
             ALTER TABLE "categories"
             ADD CONSTRAINT "FK_de08738901be6b34d2824a1e243" FOREIGN KEY ("parent_category_id") REFERENCES "categories"("id") ON DELETE NO ACTION ON UPDATE NO ACTION
         `);
-      await queryRunner.query(`
+    await queryRunner.query(`
             ALTER TABLE "products"
             ADD CONSTRAINT "FK_9a5f6868c96e0069e699f33e124" FOREIGN KEY ("category_id") REFERENCES "categories"("id") ON DELETE NO ACTION ON UPDATE NO ACTION
         `);
-      await queryRunner.query(`
+    await queryRunner.query(`
             ALTER TABLE "products"
             ADD CONSTRAINT "FK_ddec89ba43ccb7f143ddc18270f" FOREIGN KEY ("currency_id") REFERENCES "currencies"("id") ON DELETE NO ACTION ON UPDATE NO ACTION
         `);
-      await queryRunner.query(`
+    await queryRunner.query(`
             ALTER TABLE "products"
             ADD CONSTRAINT "FK_1530a6f15d3c79d1b70be98f2be" FOREIGN KEY ("brand_id") REFERENCES "brands"("id") ON DELETE NO ACTION ON UPDATE NO ACTION
         `);
-      await queryRunner.query(`
+    await queryRunner.query(`
             ALTER TABLE "carts"
             ADD CONSTRAINT "FK_ace88a18b5f57ade458db9061d2" FOREIGN KEY ("order_id") REFERENCES "orders"("id") ON DELETE NO ACTION ON UPDATE NO ACTION
         `);
-      await queryRunner.query(`
+    await queryRunner.query(`
             ALTER TABLE "payments"
             ADD CONSTRAINT "FK_b2f7b823a21562eeca20e72b006" FOREIGN KEY ("order_id") REFERENCES "orders"("id") ON DELETE NO ACTION ON UPDATE NO ACTION
         `);
-      await queryRunner.query(`
+    await queryRunner.query(`
             ALTER TABLE "product_images"
             ADD CONSTRAINT "FK_5dcb680c005c847a5ac7fbc119b" FOREIGN KEY ("original_attachment_id") REFERENCES "attachments"("id") ON DELETE NO ACTION ON UPDATE NO ACTION
         `);
-      await queryRunner.query(`
+    await queryRunner.query(`
             ALTER TABLE "product_images"
             ADD CONSTRAINT "FK_81ab0379de4192c1311f032c39f" FOREIGN KEY ("small_attachment_id") REFERENCES "attachments"("id") ON DELETE NO ACTION ON UPDATE NO ACTION
         `);
-      await queryRunner.query(`
+    await queryRunner.query(`
             ALTER TABLE "product_properties"
             ADD CONSTRAINT "FK_7fad253874cd58cb759099147a7" FOREIGN KEY ("product_id") REFERENCES "products"("id") ON DELETE CASCADE ON UPDATE CASCADE
         `);
-      await queryRunner.query(`
+    await queryRunner.query(`
             ALTER TABLE "product_properties"
             ADD CONSTRAINT "FK_0f3885b4091f332d3476181e0c0" FOREIGN KEY ("property_id") REFERENCES "properties"("id") ON DELETE CASCADE ON UPDATE CASCADE
         `);
-      await queryRunner.query(`
+    await queryRunner.query(`
             ALTER TABLE "cart_products"
             ADD CONSTRAINT "FK_ebc4fe8eabf38786bb86cda0b9f" FOREIGN KEY ("cart_id") REFERENCES "carts"("id") ON DELETE CASCADE ON UPDATE CASCADE
         `);
-      await queryRunner.query(`
+    await queryRunner.query(`
             ALTER TABLE "cart_products"
             ADD CONSTRAINT "FK_bb7910594db3f08cadeb6f484c1" FOREIGN KEY ("product_id") REFERENCES "products"("id") ON DELETE CASCADE ON UPDATE CASCADE
         `);
-   }
+  }
 
-   public async down(queryRunner: QueryRunner): Promise<void> {
-      await queryRunner.query(`
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(`
             ALTER TABLE "cart_products" DROP CONSTRAINT "FK_bb7910594db3f08cadeb6f484c1"
         `);
-      await queryRunner.query(`
+    await queryRunner.query(`
             ALTER TABLE "cart_products" DROP CONSTRAINT "FK_ebc4fe8eabf38786bb86cda0b9f"
         `);
-      await queryRunner.query(`
+    await queryRunner.query(`
             ALTER TABLE "product_properties" DROP CONSTRAINT "FK_0f3885b4091f332d3476181e0c0"
         `);
-      await queryRunner.query(`
+    await queryRunner.query(`
             ALTER TABLE "product_properties" DROP CONSTRAINT "FK_7fad253874cd58cb759099147a7"
         `);
-      await queryRunner.query(`
+    await queryRunner.query(`
             ALTER TABLE "product_images" DROP CONSTRAINT "FK_81ab0379de4192c1311f032c39f"
         `);
-      await queryRunner.query(`
+    await queryRunner.query(`
             ALTER TABLE "product_images" DROP CONSTRAINT "FK_5dcb680c005c847a5ac7fbc119b"
         `);
-      await queryRunner.query(`
+    await queryRunner.query(`
             ALTER TABLE "payments" DROP CONSTRAINT "FK_b2f7b823a21562eeca20e72b006"
         `);
-      await queryRunner.query(`
+    await queryRunner.query(`
             ALTER TABLE "carts" DROP CONSTRAINT "FK_ace88a18b5f57ade458db9061d2"
         `);
-      await queryRunner.query(`
+    await queryRunner.query(`
             ALTER TABLE "products" DROP CONSTRAINT "FK_1530a6f15d3c79d1b70be98f2be"
         `);
-      await queryRunner.query(`
+    await queryRunner.query(`
             ALTER TABLE "products" DROP CONSTRAINT "FK_ddec89ba43ccb7f143ddc18270f"
         `);
-      await queryRunner.query(`
+    await queryRunner.query(`
             ALTER TABLE "products" DROP CONSTRAINT "FK_9a5f6868c96e0069e699f33e124"
         `);
-      await queryRunner.query(`
+    await queryRunner.query(`
             ALTER TABLE "categories" DROP CONSTRAINT "FK_de08738901be6b34d2824a1e243"
         `);
-      await queryRunner.query(`
+    await queryRunner.query(`
             ALTER TABLE "orders" DROP CONSTRAINT "FK_772d0ce0473ac2ccfa26060dbe9"
         `);
-      await queryRunner.query(`
+    await queryRunner.query(`
             ALTER TABLE "brands" DROP CONSTRAINT "FK_1d6279240938040736d83064356"
         `);
-      await queryRunner.query(`
+    await queryRunner.query(`
             DROP INDEX "public"."IDX_bb7910594db3f08cadeb6f484c"
         `);
-      await queryRunner.query(`
+    await queryRunner.query(`
             DROP INDEX "public"."IDX_ebc4fe8eabf38786bb86cda0b9"
         `);
-      await queryRunner.query(`
+    await queryRunner.query(`
             DROP TABLE "cart_products"
         `);
-      await queryRunner.query(`
+    await queryRunner.query(`
             DROP INDEX "public"."IDX_0f3885b4091f332d3476181e0c"
         `);
-      await queryRunner.query(`
+    await queryRunner.query(`
             DROP INDEX "public"."IDX_7fad253874cd58cb759099147a"
         `);
-      await queryRunner.query(`
+    await queryRunner.query(`
             DROP TABLE "product_properties"
         `);
-      await queryRunner.query(`
+    await queryRunner.query(`
             DROP TABLE "product_images"
         `);
-      await queryRunner.query(`
+    await queryRunner.query(`
             DROP TABLE "payments"
         `);
-      await queryRunner.query(`
+    await queryRunner.query(`
             DROP TABLE "carts"
         `);
-      await queryRunner.query(`
+    await queryRunner.query(`
             DROP TABLE "products"
         `);
-      await queryRunner.query(`
+    await queryRunner.query(`
             DROP TABLE "properties"
         `);
-      await queryRunner.query(`
+    await queryRunner.query(`
             DROP TYPE "public"."properties_type_enum"
         `);
-      await queryRunner.query(`
+    await queryRunner.query(`
             DROP TABLE "currencies"
         `);
-      await queryRunner.query(`
+    await queryRunner.query(`
             DROP TABLE "categories"
         `);
-      await queryRunner.query(`
+    await queryRunner.query(`
             DROP TABLE "orders"
         `);
-      await queryRunner.query(`
+    await queryRunner.query(`
             DROP TYPE "public"."orders_status_enum"
         `);
-      await queryRunner.query(`
+    await queryRunner.query(`
             DROP TABLE "users"
         `);
-      await queryRunner.query(`
+    await queryRunner.query(`
             DROP TABLE "brands"
         `);
-      await queryRunner.query(`
+    await queryRunner.query(`
             DROP TABLE "attachments"
         `);
-      await queryRunner.query(`
+    await queryRunner.query(`
             DROP TYPE "public"."attachments_type_enum"
         `);
-   }
+  }
 }
--- src/migrations/1643006090428-oneUserSeed.ts
+++ src/migrations/1643006090428-oneUserSeed.ts
@@ -1,11 +1,11 @@
-import { MigrationInterface, QueryRunner } from "typeorm";
+import { MigrationInterface, QueryRunner } from 'typeorm';
 
 export class oneUserSeed1643006090428 implements MigrationInterface {
-   public async up(queryRunner: QueryRunner): Promise<void> {
-      await queryRunner.query(
-         `INSERT INTO USERS (email, phone, first_name, last_name, password) VALUES ('some@mail.com', '79000000000', 'Name', 'LastName', '$2b$07$2ZNd7n90N4.yqmKcQKPbV.Ih8X7vEJbXRrIr/5IIPM5JuUBsI2jrm');`
-      ); // hash - "password"
-   }
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(
+      `INSERT INTO USERS (email, phone, first_name, last_name, password) VALUES ('some@mail.com', '79000000000', 'Name', 'LastName', '$2b$07$2ZNd7n90N4.yqmKcQKPbV.Ih8X7vEJbXRrIr/5IIPM5JuUBsI2jrm');`,
+    ); // hash - "password"
+  }
 
-   public async down(queryRunner: QueryRunner): Promise<void> {}
+  public async down(queryRunner: QueryRunner): Promise<void> {}
 }
