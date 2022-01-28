13ac5bd316a1fbd84ee7e47a60562c1caf85621d
ajiways <ajiwayswc@gmail.com>
2022-01-29 01:46:34 +0700

    Конфиги для typeorm

---
+++ src/config/typeorm.ts
@@ -0,0 +1,33 @@
+import { SnakeNamingStrategy } from 'typeorm-naming-strategies';
+import { ConfigService } from '@nestjs/config';
+import {
+  TypeOrmModuleAsyncOptions,
+  TypeOrmModuleOptions,
+} from '@nestjs/typeorm';
+import { join } from 'path';
+
+const configService = new ConfigService();
+
+export function getOrmConfig(): TypeOrmModuleOptions {
+  return {
+    type: 'postgres',
+    host: configService.get('PG_HOST'),
+    port: configService.get<number>('PG_PORT'),
+    username: configService.get('PG_USER'),
+    password: configService.get('PG_PASSWORD'),
+    database: configService.get('PG_DATABASE'),
+    namingStrategy: new SnakeNamingStrategy(),
+    entities: [join(__dirname, '..', '/modules', '/**', '/*.entity.{ts,js}')],
+    synchronize: false,
+    migrations: [join(__dirname, '..', '/migrations', '*.{ts,js}')],
+    migrationsTableName: 'migrations',
+    logging: 'all',
+    cli: {
+      migrationsDir: join(__dirname, '..', '/migrations', '/*.{ts,js}'),
+    },
+  };
+}
+
+export const typeOrmConfigAsync: TypeOrmModuleAsyncOptions = {
+  useFactory: async (): Promise<TypeOrmModuleOptions> => getOrmConfig(),
+};
