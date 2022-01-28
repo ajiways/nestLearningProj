77f4057a481cd549ebd552dc4f50f859a93fdf7c
ajiways <ajiwayswc@gmail.com>
2022-01-29 02:01:34 +0700

    Забыл реализовать контроллер для User

--- src/modules/user/user.controller.ts
+++ src/modules/user/user.controller.ts
@@ -1,4 +1,15 @@
 import { Controller } from '@nestjs/common';
+import { UserService } from './user.service';
 
 @Controller('users')
-export class UserController {}
+export class UserController {
+  constructor(private readonly userService: UserService) {}
+
+  async getAllUsers() {
+    return await this.userService.findAll();
+  }
+
+  async getUserById(id: number) {
+    return await this.userService.findOne(id);
+  }
+}
