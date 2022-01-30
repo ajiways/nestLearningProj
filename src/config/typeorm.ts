99af5a59094bb46e9b5b8d47bf4bcbf803a4aa50
ajiways <ajiwayswc@gmail.com>
2022-01-31 05:21:03 +0700

    фиксы путей в typeorm конфиге

--- src/config/typeorm.ts
+++ src/config/typeorm.ts
@@ -19,11 +19,11 @@ export function getOrmConfig(): TypeOrmModuleOptions {
     namingStrategy: new SnakeNamingStrategy(),
     entities: [join(__dirname, '..', '/modules', '/**', '/*.entity.{ts,js}')],
     synchronize: false,
-    migrations: [join(__dirname, '..', '/migrations', '*.{ts,js}')],
+    migrations: [join(__dirname, '..', '/migrations', '/*.{ts,js}')],
     migrationsTableName: 'migrations',
     logging: 'all',
     cli: {
-      migrationsDir: join(__dirname, '..', '/migrations', '/*.{ts,js}'),
+      migrationsDir: join(__dirname, '..', '/migrations'),
     },
   };
 }
