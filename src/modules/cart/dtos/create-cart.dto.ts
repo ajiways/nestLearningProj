63291b8a8d79b0eb51574686f533cd5678198cc3
ajiways <ajiwayswc@gmail.com>
2022-02-01 18:00:33 +0700

    микрофиксы

--- src/modules/cart/dtos/create-cart.dto.ts
+++ src/modules/cart/dtos/create-cart.dto.ts
@@ -1,15 +1,12 @@
-import { IsNumber, Min } from 'class-validator';
+import { Min } from 'class-validator';
 
 export class CreateCartDto {
-  @IsNumber({ allowNaN: false })
   @Min(1)
   customerId: number;
 
-  @IsNumber({ allowNaN: false })
   @Min(1)
   productId: number;
 
-  @IsNumber({ allowNaN: false })
   @Min(1)
   amount: number;
 }
--- src/modules/order/dtos/create-order.dto.ts
+++ src/modules/order/dtos/create-order.dto.ts
@@ -1,6 +1,10 @@
+import { IsEnum, Min } from 'class-validator';
 import { orderStatus } from '../order.entity';
 
 export class CreateOrderDto {
+  @Min(0)
   customerId: number;
+
+  @IsEnum(orderStatus)
   status: orderStatus.REGISTRATION;
 }
